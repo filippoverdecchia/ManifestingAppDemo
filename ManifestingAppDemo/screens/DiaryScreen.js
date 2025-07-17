c81bf722ff4b9e26a43e633f5a83f26c4c87f635
filippoverdecchia <filippo.verdecchia@studenti.unicam.it>
2025-07-17 12:25:05 +0200

    Aggiungo la cartella del progetto

---
+++ ManifestingAppDemo/screens/DiaryScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function DiaryScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>​​📚​​​ Questa è la schermata del Diario</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+  },
+});
